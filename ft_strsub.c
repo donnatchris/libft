cad64d68cab868984b89e66c2a8eb0a28dafcc5e
chdonnat <donnatchris@live.fr>
2024-10-17 11:08:28 +0200

    added ft_strdel, ft_strjoin, ft_strnew, ft_strsub, ft_strtrim ok

---
+++ ft_strsub.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char	*cpy;
+	size_t	i;
+
+	cpy = malloc((len + 1) * sizeof(char));
+	if (cpy == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		cpy[i] = s[i + start];
+		i++;
+	}
+	cpy[i] = '\0';
+	return (cpy);
+}
---
+++ ft_strtrim.c
@@ -0,0 +1,52 @@
+#include "libft.h"
+
+int	ft_is_white_space(char c)
+{
+	return (c == ' ' || c == '\n' || c == '\t');
+}
+
+size_t	ft_trim_len(const char *s)
+{
+	size_t	i;
+	size_t	j;
+	size_t	len;
+
+	len = ft_strlen(s);
+	if (len == 0)
+		return (0);
+	i = 0;
+	while (ft_is_white_space(s[i]))
+		i++;
+	if (i == len)
+		return (0);
+	j = 0;
+	while (ft_is_white_space(s[len - j - 1]) && (len - j) > 0)
+		j++;
+	return (len - i - j);
+}
+
+char	*ft_strtrim(char const *s)
+{
+	char	*trim;
+	size_t	len;
+	size_t	i;
+	size_t	j;
+
+	if (s == NULL)
+		return (NULL);
+	len = ft_trim_len(s);
+	trim = malloc((len + 1) * sizeof(char));
+	if (trim == NULL)
+		return (NULL);
+	i = 0;
+	while (ft_is_white_space(s[i]))
+		i++;
+	j = 0;
+	while (j < len)
+	{
+		trim[j] = s[i + j];
+		j++;
+	}
+	trim[j] = '\0';
+	return (trim);
+}

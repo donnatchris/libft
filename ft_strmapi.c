53707acd713795d76b5f44a877262bbf629cea3a
chdonnat <donnatchris@live.fr>
2024-10-22 10:34:13 +0200

    added ft_strmapi ok, part 1 and 2 complete but corrections needed: remove some locals ft_strlen and stratic

---
+++ ft_strmapi.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*new;
+	size_t	i;
+
+	if (s == NULL || f == NULL)
+		return (NULL);
+	new = ft_strnew(ft_strlen(s));
+	if (new == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		new[i] = f(i, s[i]);
+		i++;
+	}
+	return (new);
+}

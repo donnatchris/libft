1796c25af397823acf79e5c9396b60d6feae6675
chdonnat <donnatchris@live.fr>
2024-10-22 09:53:21 +0200

    added ft_striteri ok

--- libft.h
+++ libft.h
@@ -45,6 +45,7 @@ void	ft_strclr(char *s);
 void	ft_memdel(void **ap);
 void	ft_strdel(char **as);
 void	ft_striter(char *s, void (*f)(char *));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);

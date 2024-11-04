309bce81960175048991905b7b40b923fd0dd60f
chdonnat <donnatchris@live.fr>
2024-11-04 23:08:18 +0100

    correction of libft_bonus.h

--- libft_bonus.h
+++ libft_bonus.h
@@ -26,9 +26,9 @@ LIST MANIPULATION FUNCTIONS
 ******************************************************************************/
 
 t_list	*ft_lstnew(void *content);
-//	Allocates (using malloc()) and returns a "fresh" link.
-//		The content field in the new link is initialized by copying the
-//		function parameter.
+//	Allocates (using malloc()) and returns a new link.
+//		The content field in the new link is initialized with the value of the
+//		content parameter.
 //		The next field is initialized to NULL.
 //	-> Returns the "fresh" link, or NULL if allocation fails.
 
@@ -39,15 +39,15 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *));
 
 void	ft_lstadd_front(t_list **lst, t_list *new);
 //	Add the element new at the start of the list
-//		(lst is the adress of the first element of the list,
-//		and new is the adress of the pointer to the element to be added).
+//		(lst is the address of the first element of the list,
+//		and new is the address of the pointer to the element to be added).
 
 int		ft_lstsize(t_list *lst);
 //	Counts the number of elements of the list.
 //		Lst is the start of the list.
 //	-> Returns the count.
 
-t_list	ft_lstlast(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 //	Finds the last element of the list (where lst is the first element).
 //	-> Returns the last element of the list.
 
